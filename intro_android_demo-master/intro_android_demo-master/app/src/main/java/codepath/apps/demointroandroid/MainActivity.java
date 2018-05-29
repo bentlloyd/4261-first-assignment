a896aa6b6fd30bf0b9a2d0563d4b012286127f8d
bentlloyd <bentlloyd@gatech.edu>
2018-05-28 20:13:42 -0400

    Add twitter authentication

--- intro_android_demo-master/intro_android_demo-master/app/src/main/java/codepath/apps/demointroandroid/MainActivity.java
+++ intro_android_demo-master/intro_android_demo-master/app/src/main/java/codepath/apps/demointroandroid/MainActivity.java
@@ -4,8 +4,10 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
@@ -14,17 +16,39 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.twitter.sdk.android.core.Callback;
+import com.twitter.sdk.android.core.DefaultLogger;
+import com.twitter.sdk.android.core.Result;
+import com.twitter.sdk.android.core.Twitter;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.TwitterAuthToken;
+import com.twitter.sdk.android.core.TwitterConfig;
+import com.twitter.sdk.android.core.TwitterCore;
+import com.twitter.sdk.android.core.TwitterException;
+import com.twitter.sdk.android.core.TwitterSession;
+import com.twitter.sdk.android.core.identity.TwitterLoginButton;
 
 public class MainActivity extends Activity implements View.OnClickListener {
 
     FirebaseAuth mAuth;
     EditText editTextEmail, editTextPassword;
     ProgressBar progressBar;
+    TwitterLoginButton loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Twitter.initialize(this);
         setContentView(R.layout.activity_main);
+
+        TwitterConfig config = new TwitterConfig.Builder(this)
+                .logger(new DefaultLogger(Log.DEBUG))
+                .twitterAuthConfig(new TwitterAuthConfig(
+                        getString(R.string.com_twitter_sdk_android_CONSUMER_KEY),
+                        getString(R.string.com_twitter_sdk_android_CONSUMER_SECRET)))
+                .debug(true)
+                .build();
+        Twitter.initialize(config);
 
         mAuth = FirebaseAuth.getInstance();
 
@@ -35,6 +59,52 @@ public class MainActivity extends Activity implements View.OnClickListener {
         findViewById(R.id.textViewSignup).setOnClickListener(this);
         findViewById(R.id.buttonLogin).setOnClickListener(this);
 
+        loginButton = (TwitterLoginButton) findViewById(R.id.loginBut);
+        loginButton.setCallback(new Callback<TwitterSession>() {
+            @Override
+            public void success(Result<TwitterSession> result) {
+                /*
+                  This provides TwitterSession as a result
+                  This will execute when the authentication is successful
+                 */
+                TwitterSession session = TwitterCore.getInstance().getSessionManager().getActiveSession();
+                TwitterAuthToken authToken = session.getAuthToken();
+                String token = authToken.token;
+                String secret = authToken.secret;
+
+                //Calling login method and passing twitter session
+                login(session);
+            }
+
+            @Override
+            public void failure(TwitterException exception) {
+                //Displaying Toast message
+                Toast.makeText(MainActivity.this, "Authentication failed!", Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+    }
+
+    /**
+     * @param session
+     * This method will get username using session and start a new activity where username will be displayed
+     */
+    public void login(TwitterSession session)
+    {
+        String username = session.getUserName();
+        Intent intent = new Intent(MainActivity.this, DemoSelector.class);
+        intent.putExtra("username", username);
+        startActivity(intent);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Pass the activity result to the login button.
+        loginButton.onActivityResult(requestCode, resultCode, data);
     }
 
     private void userLogin() {
